77618f4de8488f4bdf574fb6bfbdc9dd3ad013a1
muhammad rayhanul ihsan <145965961+rayhanul-ihsan@users.noreply.github.com>
2024-01-19 22:22:14 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 import './main.css';
-import './bootstrap/css/bootstrap.min.css';
+import './bootstrap/css/bootstrap.css';
 
 import { BrowserRouter } from 'react-router-dom';
 
